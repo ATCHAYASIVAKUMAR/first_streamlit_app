846df7edc800470394e662820afd9417f9ffa19a
ATCHAYASIVAKUMAR <113783785+ATCHAYASIVAKUMAR@users.noreply.github.com>
2022-09-17 22:19:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast menu')
-streamlit.text('chicken briyani and lollipop')
+streamlit.header(' Breakfast Favourites')
+
+streamlit.text('😜😜chicken briyani and lollipop')
 streamlit.text('kalaki egg')
