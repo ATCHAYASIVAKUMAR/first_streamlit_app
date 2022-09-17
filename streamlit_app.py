a2e62ce5a78b26f71d18b887c7541517fb3f6b62
ATCHAYASIVAKUMAR <113783785+ATCHAYASIVAKUMAR@users.noreply.github.com>
2022-09-17 22:28:18 +0530

    Update streamlit_app.py
    importing pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ streamlit.header(' Breakfast Favourites')
 
 streamlit.text('😜😜chicken briyani and lollipop')
 streamlit.text('kalaki egg')
+
+streamlit.header('Lunch Favourites of the day')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
