9cda6711a4f70c3989d69b8afae0d183fb032f5f
ATCHAYASIVAKUMAR <113783785+ATCHAYASIVAKUMAR@users.noreply.github.com>
2022-09-17 22:13:51 +0530

    Update streamlit_app.py
    just extra food typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('chicken briyani and lollipop')
+Streamlit.text('kalaki egg')
